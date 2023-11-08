35aa28438e289433542ffbc507da941be969ede1
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-08 14:49:38 -0800

    nav component started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
-// import Nav from './components/Nav';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div>
+      <Nav />
       <Outlet />
     </div>
   );
---
+++ src/components/Nav.jsx
@@ -0,0 +1,5 @@
+import { Link } from 'react-router-dom';
+
+export default function Nav() {
+  return <h1>Nav</h1>;
+};
