d49f64ed44169b575c8a1bf340e19cf34bb419e8
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-09 14:37:03 -0800

    fixed contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,24 +5,24 @@ export default function Contact() {
         <div id="body-style" className="min-w-screen m-5 flex flex-column justify-center align-center">
             <Header header='Contact' />
 
-            <div id="contact-form" className="my-3 p-3 w-1/2 m-auto overflow-auto border rounded-lg">
+            <div id="contact-form" className="my-3 p-3 m-auto overflow-auto border rounded-lg">
                 <form>
-                    <div id="form-input" className="flex flex-row justify-between m-3">
+                    <div id="form-input" className="flex flex-column justify-between m-3">
                         <label for="name" className="text-white">Name: </label>
-                        <input type="text" name="name" className="p-2 min-w-max" size="40"></input>          
+                        <input type="text" name="name" className="p-2 min-w-max" size="40" required></input>          
                     </div>
 
-                    <div id="form-input" className="flex flex-row justify-between m-3">
+                    <div id="form-input" className="flex flex-column justify-between m-3">
                         <label for="email" className="text-white">Email: </label>
-                        <input type="text" name="email" className="p-2 min-w-max" size="40"></input>          
+                        <input type="text" name="email" className="p-2 min-w-max" size="40" required></input>          
                     </div>
                    
-                    <div id="form-input" className="flex flex-row justify-between m-3">
+                    <div id="form-input" className="flex flex-column justify-between m-3">
                         <label for="name" className="text-white">Message: </label>
-                        <textarea placeholder="Enter your message here..." cols="39" rows="5" className="p-2 min-w-max"></textarea>       
+                        <textarea placeholder="Enter your message here..." cols="39" rows="5" className="p-2 min-w-max" required></textarea>       
                     </div>
 
-                    <div id="form-input" className="flex flex-row m-auto">
+                    <div id="form-input" className="flex flex-column m-auto">
                         <input type="submit" value="Submit" className="text-white border rounded-lg m-auto p-2 hover:bg-slate-900 "></input>
                     </div>
                     
