fa6bfc27ad75e14d8bf4e32191c8450b8ffc3268
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-09 14:32:05 -0800

    portfolio page done

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,13 @@
 export default function Project({ name, screenshot, github, deploy }) {
     return (
-        <div id="project-card" className="m-3">
-            <img src={screenshot} width="600"/>
+        <div id="project-card" className="m-3 flex flex-column text-center text-white ">
+            <h2 className="text-xl mb-1">{name}</h2>    
+            <img id="project-img" src={screenshot} width="600"/>
+
+            <div id="project-text" className="flex flex-row justify-evenly mt-1">
+                <a href={github}><h3>Github Repo</h3></a>
+                <a href={deploy}><h3>Deployed Project</h3></a>
+            </div>  
         </div>
     );
 }
