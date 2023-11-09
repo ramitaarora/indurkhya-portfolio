fe66bf4517e4db7044ff0c837d8ec61d0291f706
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-08 21:01:23 -0800

    footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Nav />
       <Outlet />
+      <Footer />
     </div>
   );
 }
