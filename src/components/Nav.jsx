d5c9a80acb03d431bf466204d7f820f05058c545
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-08 18:38:42 -0800

    navbar basic styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,13 @@
 import { Link } from 'react-router-dom';
 
 export default function Nav() {
-  return <h1>Nav</h1>;
+  return (
+    <div className="min-w-full flex flex-row justify-evenly bg-gray-700 text-white p-3">
+            <Link classname="cursor-pointer" to="/">Home</Link>
+            <Link classname="cursor-pointer" to="/About">About</Link>
+            <Link classname="cursor-pointer" to="/Portfolio">Portfolio</Link>
+            <Link classname="cursor-pointer" to="/Contact">Contact</Link>
+
+    </div>
+  );
 };
