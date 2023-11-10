fd15ebdce64b1ded7a2d36b1ad04da5f0dd7de39
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-09 18:53:24 -0800

    fixed highlighting with nav links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Nav() {
+
   return (
     <nav className="flex flex-row justify-between bg-gray-800 text-white p-3">
     
@@ -9,11 +10,11 @@ export default function Nav() {
       </div>
       
       <div className="w-96 flex flex-row align-center justify-evenly">
-        <Link className="cursor-pointer" to="/">Home</Link>
-        <Link className="cursor-pointer" to="/About">About</Link>
-        <Link className="cursor-pointer" to="/Portfolio">Portfolio</Link>
-        <Link className="cursor-pointer" to="/Contact">Contact</Link>
-        <Link className="cursor-pointer" to="/Resume">Resume</Link>
+        <NavLink activeClassName="active" to="/">Home</NavLink>
+        <NavLink activeClassName="active" to="/About">About</NavLink>
+        <NavLink to="/Portfolio">Portfolio</NavLink>
+        <NavLink to="/Contact">Contact</NavLink>
+        <NavLink to="/Resume">Resume</NavLink>
       </div>
             
     </nav>
