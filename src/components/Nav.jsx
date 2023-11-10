e79087df08fa93a677614185aa9608c057a5f56d
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-09 21:06:41 -0800

    responsive nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,13 +3,13 @@ import { NavLink } from 'react-router-dom';
 export default function Nav() {
 
   return (
-    <nav className="flex flex-row justify-between bg-gray-800 text-white p-3">
+    <nav className="bg-gray-800 text-white p-3 flex flex-row justify-between">
     
       <div>
         <p>Ramita's Portfolio</p>
       </div>
       
-      <div className="w-96 flex flex-row align-center justify-evenly">
+      <div className="w-96 flex flex-row align-center justify-evenly flex-wrap">
         <NavLink activeClassName="active" to="/">Home</NavLink>
         <NavLink activeClassName="active" to="/About">About</NavLink>
         <NavLink activeClassName="active" to="/Portfolio">Portfolio</NavLink>
