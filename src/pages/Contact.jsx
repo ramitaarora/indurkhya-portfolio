436a0c53b2c292a25179d345bb84d13bd5ddf573
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-09 17:20:04 -0800

    email validation fixed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,36 @@
 import Header from '../components/Header';
 
 export default function Contact() {
+    const onSubmit = (event) => {
+        event.preventDefault();
+        let regex = new RegExp("^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$");
+        let valid = regex.test(event.target[1].value);
+        if (valid) {
+            window.alert("Your message has been sent!")
+            location.reload();
+        } else {
+            window.alert("Please enter a valid email.")
+        }
+    }
+
     return (
         <div id="body-style" className="min-w-screen m-5 flex flex-column justify-center align-center">
             <Header header='Contact' />
 
             <div id="contact-form" className="my-3 p-3 m-auto overflow-auto border rounded-lg">
-                <form>
+                <form onSubmit={() => onSubmit(event)}>
                     <div id="form-input" className="flex flex-column justify-between m-3">
-                        <label for="name" className="text-white">Name: </label>
+                        <label htmlFor="name" className="text-white">Name: </label>
                         <input type="text" name="name" className="p-2 min-w-max" size="40" required></input>          
                     </div>
 
                     <div id="form-input" className="flex flex-column justify-between m-3">
-                        <label for="email" className="text-white">Email: </label>
-                        <input type="text" name="email" className="p-2 min-w-max" size="40" pattern="/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/" required></input>          
+                        <label htmlFor="email" className="text-white">Email: </label>
+                        <input type="text" name="email" className="p-2 min-w-max" size="40" required></input>          
                     </div>
                    
                     <div id="form-input" className="flex flex-column justify-between m-3">
-                        <label for="name" className="text-white">Message: </label>
+                        <label htmlFor="name" className="text-white">Message: </label>
                         <textarea placeholder="Enter your message here..." cols="39" rows="5" className="p-2 min-w-max" required></textarea>       
                     </div>
 
