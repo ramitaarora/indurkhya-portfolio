dd25dd9386fca5ee4c81a87a09777802ce621f2d
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-09 17:25:10 -0800

    error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,3 +1,15 @@
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
+
 export default function Error() {
-   return <h1>Error</h1>   ;
+   return (
+      <>
+         <Nav />
+            <div className="text-white text-3xl min-h-full text-center">
+               <h1>Error!</h1>
+            </div>
+         <Footer />
+      </>
+     
+   )
 }
