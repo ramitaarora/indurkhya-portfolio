7f4933342ac81e4753ccfb22bb7e5dd7f6a2b14c
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-05-01 10:21:57 -0700

    updated resume link, portfolio projects and header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div id="home">
-            <div id="logo-parent" className="flex flex-row min-w-screen md:min-h-screen min-h-full justify-center items-center">
+            <div id="logo-parent" className="flex flex-row min-w-screen md:min-h-80 justify-center items-center">
                 <div id="logo-div" className="hover:animate-bounce">
                     <p id="logo">Ramita Indurkhya</p>
                     <p id="tagline">Web Developer</p>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,6 +3,14 @@ import Project from '../components/Project';
 
 let projects = [
     {
+        name: "Mosaic Planner",
+        screenshot: "/images/planner-screenshot.png",
+        github: "https://github.com/ramitaarora/mosaic-planner",
+        deploy: "https://mosaic-planner-112e49ce3b90.herokuapp.com/",
+        description: `This project is a full-stack application that combines different aspects of a traditional planner: a calendar, schedule, daily items, reminders/notes, and goals. A user will be able to add, view, update and delete anything via the dashboard.`,
+        tech: ['React', 'JavaScript', 'MySQL', 'Sequelize', 'Node', 'Express']
+    },
+    {
         name: "Meal Match",
         screenshot: "/images/meal-match-screenshot.png",
         github: "https://github.com/ramitaarora/meal-match",
@@ -17,22 +25,6 @@ let projects = [
         deploy: "https://chit-chat-wechatalot-00f4f8dc2d8e.herokuapp.com/",
         description: `A full-stack web chat application where a user may sign up and log in, edit their profile, add friends, chat with friends. As a collaboration project, Ramita contributed by building the database with routes and controllers from the front end to the back end. She also set up the web socket to display messages in real time to different users, as well as save the chat history.`,
         tech: ['React', 'Node.js', 'Apollo GraphQL', 'MongoDB', 'Socket.io']
-    },
-    {
-        name: "Mosaic Planner",
-        screenshot: "/images/planner-screenshot.png",
-        github: "https://github.com/ramitaarora/mosaic-planner",
-        deploy: "https://mosaic-planner-112e49ce3b90.herokuapp.com/",
-        description: `This project is a full-stack application that combines different aspects of a traditional planner: a calendar, schedule, daily items, reminders/notes, and goals. A user will be able to add, view, update and delete anything via the dashboard.`,
-        tech: ['React', 'JavaScript', 'MySQL', 'Sequelize', 'Node', 'Express']
-    },
-    {
-        name: "Tech Blog",
-        screenshot: "/images/tech-blog-screenshot.png",
-        github: "https://github.com/ramitaarora/mvc-tech-blog",
-        deploy: "https://mvc-tech-blog-indurkhya-ecc84c878b3c.herokuapp.com/",
-        description: `This project is a full-stack CRUD website, similar to WordPress and other CMS websites. Users may sign up and log in to write, edit and delete blog posts. They may also comment on other user's blog posts.`,
-        tech: ['JavaScript', 'CSS', 'SQL', 'MySQL', 'Sequelize']
     }
 ]
 
@@ -41,7 +33,7 @@ export default function Portfolio() {
         <div id="portfolio" className="min-w-screen m-4 flex flex-column justify-center align-center">
             <Header header='Portfolio' /> 
 
-            <div id="project-cards" className="min-w-screen mt-3 flex flex-row flex-wrap justify-center align-center">
+            <div id="project-cards" className="min-w-screen flex flex-row flex-wrap justify-center align-center">
             {projects.map((item, index) => {
                 return <Project key={index} name={item.name} screenshot={item.screenshot} github={item.github} deploy={item.deploy} description={item.description} tech={item.tech}/>
             })}
